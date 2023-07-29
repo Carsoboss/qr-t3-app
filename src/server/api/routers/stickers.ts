f13140f7dbb58c621bf6b81d1813bb56c24b8d41
Carson Karren <carsonkarren@carsons-mbp.lan>
2023-07-29 12:17:30 -0600

    added stickers and users endpoints.  Not done.

--- src/server/api/routers/stickers.ts
+++ src/server/api/routers/stickers.ts
@@ -2,17 +2,25 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/server";
-
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import type { Sticker } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+import type { UserTenet } from "@prisma/client";
 
 const filterUserForClient = (user: User) => {
   return {
     id: user.id,
   };
 };
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(15, "1 m"),
+  analytics: true,
+});
+
 const addUserDataToStickers = async (stickers: Sticker[]) => {
-  const userId = stickers.map((sticker) => sticker.userTenetId);
+  const userId = stickers.map((sticker) => sticker.ownerId);
   const users = (
     await clerkClient.users.getUserList({
       userId: userId,
@@ -21,25 +29,30 @@ const addUserDataToStickers = async (stickers: Sticker[]) => {
   ).map(filterUserForClient);
 
   return stickers.map((sticker) => {
-    const stickerOwner = users.find((user) => user.id === sticker.userTenetId);
+    const stickerOwner = users.find((user) => user.id === sticker.ownerId);
 
     if (!stickerOwner) {
       console.error("AUTHOR NOT FOUND", sticker);
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
-        message: `Owner of sticker not found: ${sticker.id}, USER ID: ${sticker.userTenetId}`,
+        message: `Owner of sticker not found: ${sticker.id}, USER ID: ${sticker.ownerId}`,
       });
     }
     return {
       sticker,
-      stickerOwner: {
+      owner: {
         ...stickerOwner,
       },
+      // userTenet,
+      // contacts: {
+      //   ...UserTenet,
+      // },
     };
   });
 };
 
 export const stickerRouter = createTRPCRouter({
+
   getStickersByUserId: protectedProcedure
     .input(
       z.object({
@@ -50,11 +63,71 @@ export const stickerRouter = createTRPCRouter({
       ctx.prisma.sticker
         .findMany({
           where: {
-            userTenetId: input.userId,
+            ownerId: input.userId,
           },
           take: 100,
           orderBy: [{ updatedAt: "desc" }, { createdAt: "desc" }],
         })
         .then(addUserDataToStickers)
     ),
-});
+    const create = {
+      FirstName: protectedProcedure
+        .input(
+          z.object({
+            content: z.string().min(1, "First Name Required.").max(50, "First Name is too long."),
+          })
+        )
+        .mutation(async ({ ctx, input }) => {
+          const ownerId = ctx.userId;
+    
+          const { success } = await ratelimit.limit(ownerId);
+          if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }),
+    
+      LastName: protectedProcedure
+        .input(
+          z.object({
+            content: z.string().min(1, "Last Name Required.").max(50, "Last Name is too long."),
+          })
+        )
+        .mutation(async ({ ctx, input }) => {
+          const ownerId = ctx.userId;
+    
+          const { success } = await ratelimit.limit(ownerId);
+          if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }),
+    
+      Email: protectedProcedure
+        .input(
+          z.object({
+            content: z.string().min(1, "Email Required.").max(50, "Email is too long.")
+            .refine(value => {
+              const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+              return emailRegex.test(value);
+            }, "Enter Valid Email")
+          })
+        )
+        .mutation(async ({ ctx, input }) => {
+          const ownerId = ctx.userId;
+    
+          const { success } = await ratelimit.limit(ownerId);
+          if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }),
+    
+      PhoneNumber: protectedProcedure
+        .input(
+          z.object({
+            content: z.string().min(1, "Phone Number Required.").max(50, "Phone Number is too long.")
+            .refine(value => {
+              const phoneNumberRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+              return phoneNumberRegex.test(value);
+            }, "Enter Valid Phone Number")
+          })
+        )
+        .mutation(async ({ ctx, input }) => {
+          const ownerId = ctx.userId;
+    
+          const { success } = await ratelimit.limit(ownerId);
+          if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        })
+    };
--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,23 +1,90 @@
 import { PrismaClient } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import type { ContactInfo } from "@prisma/client";
+
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 const prisma = new PrismaClient();
 
-export const userRouter = createTRPCRouter({
-  getOrCreateUser: protectedProcedure
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(15, "1 m"),
+  analytics: true,
+});
+
+const create = {
+  FirstName: protectedProcedure
     .input(
       z.object({
-        clerkUserId: z.string(),
-        firstName: z.string(),
-        lastName: z.string(),
-        email: z.string().email(),
-        phone: z.string(),
+        content: z.string().min(1, "First Name Required.").max(50, "First Name is too long."),
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const existingUser = await prisma.userTenet.findUnique({
+      const clerkUserId = ctx.userId;
+
+      const { success } = await ratelimit.limit(clerkUserId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    }),
+
+  LastName: protectedProcedure
+    .input(
+      z.object({
+        content: z.string().min(1, "Last Name Required.").max(50, "Last Name is too long."),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const clerkUserId = ctx.userId;
+
+      const { success } = await ratelimit.limit(clerkUserId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    }),
+
+  Email: protectedProcedure
+    .input(
+      z.object({
+        content: z.string().min(1, "Email Required.").max(50, "Email is too long.")
+        .refine(value => {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(value);
+        }, "Enter Valid Email")
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const clerkUserId = ctx.userId;
+
+      const { success } = await ratelimit.limit(clerkUserId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+    }),
+
+  Phone: protectedProcedure
+    .input(
+      z.object({
+        content: z.string().min(1, "Phone Number Required.").max(15, "Phone Number is too long.")
+        .refine(value => {
+          const phoneNumberRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+          return phoneNumberRegex.test(value);
+        }, "Enter Valid Phone Number")
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const clerkUserId = ctx.userId;
+
+      const { success } = await ratelimit.limit(clerkUserId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+    const addContactInfo = await ctx.prisma.contactInfo.create({
+      data: {
+        clerkUserId,
+        firstName: input.firstName,
+        lastName: input.lastName,
+        email: input.email,
+        firstName: input.phone,
+      },
+    });
+      const existingUser = await prisma.contactInfo.findUnique({
         where: { clerkUserId: input.clerkUserId },
       });
 
@@ -28,7 +95,7 @@ export const userRouter = createTRPCRouter({
       // throw or return if any field is missing and send and error to the client
 
       try {
-        const newUser = await prisma.userTenet.create({
+        const newUser = await prisma.contactInfo.create({
           data: {
             clerkUserId: input.clerkUserId,
             firstName: input.firstName,
@@ -51,7 +118,7 @@ export const userRouter = createTRPCRouter({
         });
       }
     }),
-  // rename table to be contact info instead of tenet
+
   // create get contact info endpoint. It's a query, not a mutation
   // get contact should return the data if it has all fields
 
