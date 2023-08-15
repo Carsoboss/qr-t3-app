99cb0b8620828d3e55c96a48513fa3d18af19654
Austin Karren <austin.t.karren@gmail.com>
2023-08-15 11:21:12 -0600

    added sticker id to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/:stickerId*"],
+  publicRoutes: ["/", "/sticker/:stickerId*"],
 });
 
 export const config = {
