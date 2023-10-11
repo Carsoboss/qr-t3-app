fb57c700979c73f85f25375e7f90304077b6e281
Carson Karren <carsonkarren@Carsons-MacBook-Pro.local>
2023-10-11 10:31:32 -0600

    fixed navbar as well as menu items to include clerk menu item.

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
@@ -7,11 +6,10 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import classNames from "../../utils/classNames";
-import { SignInButton, SignOutButton, clerkClient } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const navigation = [
   { name: "Home", href: "/", current: false },
-  { name: "New Item", href: "/new-item", current: false },
   { name: "My Stickers", href: "/my-stickers", current: false },
   { name: "Shop", href: "http://shop.qrfound.cool", current: false },
 ];
@@ -24,7 +22,7 @@ export default function Navbar() {
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
+                {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -68,36 +66,14 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <Menu as="div" className="relative ml-3">
-                  <Menu.Button className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span className="sr-only">Open user menu</span>
-                    <UserCircleIcon className="h-6 w-6" aria-hidden="true" />
-                  </Menu.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <div
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            <SignInButton>Sign in</SignInButton>
-                          </div>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+                <div className="text-gray-300 hover:bg-gray-700 hover:text-white">
+                  <SignedOut>
+                    <SignInButton />
+                  </SignedOut>
+                </div>
               </div>
             </div>
           </div>
