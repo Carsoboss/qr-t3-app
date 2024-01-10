a900e6c5b06c37827a4406ea203866a11a806b1c
Carson Karren <carsonkarren@Carsons-MacBook-Pro.local>
2024-01-10 12:29:00 -0700

    added on the mystickers page the component for not having any stickers

--- src/pages/my-stickers.tsx
+++ src/pages/my-stickers.tsx
@@ -66,8 +66,63 @@ export default function MyStickers() {
   if (stickersLoading) {
     return <LoadingPage />;
   }
+  if ((data ?? []).length <= 0) {
+    return (
+      <div className="flex flex-col items-center justify-start px-4 pb-40 pt-24 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md">
+          {/* Rocket icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="mx-auto h-16 w-16 text-purple-400"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+            />
+          </svg>
 
-  if (!data) return <div>Something went wrong</div>;
+          <h2 className="mt-4 pt-2 text-center text-3xl font-bold tracking-tight text-gray-900">
+            You Don&apos;t Have Any Stickers
+          </h2>
+
+          <p className="pt-6 text-center text-gray-600">
+            Scan your existing sticker to set it up <br className="sm:hidden" />
+            or buy a new sticker to get started.
+          </p>
+
+          <div className="mt-8 flex w-full justify-center">
+            <a href="https://shop.qrfound.cool">
+              <button className="rounded-md bg-violet-500 px-4 py-2 shadow-sm hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
+                {/* Sparkles icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="mr-2 inline h-6 w-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                  />
+                </svg>
+                <span className="pt-4 font-semibold text-white">
+                  Buy a Sticker Now
+                </span>
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (!data) return <div>Something went wrong</div>;
   const userEmail = user?.primaryEmailAddress?.emailAddress ?? "";
 
   const formatPhoneNumber = (phoneNumber: string): string => {
